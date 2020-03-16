2aa643e8299a723bbe8123951f8bd1ddd8c0ec0e
amansethi00 <amansethi221306@gmail.com>
2020-03-16 23:36:23 +0530

    student js not working

--- js/student.js
+++ js/student.js
@@ -1,4 +1,10 @@
 
 function studentupload(){
+    var timestamp = Number(new Date());
+    var storageRef = firebase.storage().ref(timestamp.toString());
+    var $ = jQuery;
+    const file = $('#upload').get(0).files[0]
+    var fileButton=document.getElementById('file');
     
+    storageRef.put(fileButton);
 }
