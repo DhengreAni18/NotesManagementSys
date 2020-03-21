210d77ed4c86666a1e3698851b5a3702397e8e55
DhengreAni18 <dhengreani18@gmail.com>
2020-03-21 21:52:53 +0530

    Upload functionality added

--- js/student.js
+++ js/student.js
@@ -2,9 +2,11 @@
 function studentupload(){
     var timestamp = Number(new Date());
     var storageRef = firebase.storage().ref(timestamp.toString());
-    var $ = jQuery;
-    const file = $('#upload').get(0).files[0]
-    var fileButton=document.getElementById('file');
+    var file = document.getElementById("myfile").files[0];
+    // var $ = jQuery;
+    // var file_data = $(‘#myfile’).prop(‘files’)[0];
+    // const file = $('#myFile').get(0).files[0]
+    // var fileButton=document.getElementById('file');
     
-    storageRef.put(fileButton);
+    storageRef.put(file);
 }
