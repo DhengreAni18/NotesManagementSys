daac5f7e88935eb335fae9fbb98803e35884c06a
DhengreAni18 <dhengreani18@gmail.com>
2020-03-24 00:45:32 +0530

    Big fixes

--- js/logout.js
+++ js/logout.js
@@ -1,6 +1,8 @@
 function logout() {
     firebase.auth().signOut().then(function() {
       console.log('Logout Successfull!!');
+      localStorage.removeItem("name");
+      localStorage.removeItem("key");
       window.location.replace('./signin.html')
     }).catch(function(error) {
       console.log(error);
