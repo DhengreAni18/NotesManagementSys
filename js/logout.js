0fc5382e97a88e5f053295160c4aa1fe41ba4fe6
DhengreAni18 <dhengreani18@gmail.com>
2020-03-25 14:44:51 +0530

    Bug fixes

--- js/logout.js
+++ js/logout.js
@@ -5,8 +5,9 @@ function logout() {
     .then(function() {
       console.log("Logout Successfull!!");
       localStorage.removeItem("name");
+      localStorage.removeItem("username");
       localStorage.removeItem("key");
-      window.location.replace("./signin.html");
+      window.location.replace("/signin.html");
     })
     .catch(function(error) {
       console.log(error);
